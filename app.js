95fa82b3da4225a71608cdc992897e5d3b2322e5
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-09-15 14:41:30 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,341 @@
+"use strict";
+
+var styleObj1 = {
+    color: '#f46604'
+};
+ReactDOM.render(React.createElement(
+    "center",
+    null,
+    React.createElement(
+        "div",
+        null,
+        "______________________________________________________________________________________________________________________________________________________________________________________________________________"
+    ),
+    React.createElement(
+        "table",
+        null,
+        React.createElement(
+            "tbody",
+            null,
+            React.createElement(
+                "tr",
+                null,
+                React.createElement(
+                    "td",
+                    { rowspan: "5" },
+                    React.createElement("img", { src: "img1.jpg", width: "50%", height: "50%" })
+                ),
+                React.createElement(
+                    "td",
+                    { align: "right" },
+                    "\u041D\u0430\u0437\u0432\u0430:"
+                ),
+                React.createElement(
+                    "td",
+                    { style: styleObj1 },
+                    "\u0423\u043A\u0440\u0430\u0457\u043D\u0441\u044C\u043A\u0430 \u043C\u043E\u0432\u0430. \u0411\u0443\u043A\u0432\u0430\u0440."
+                )
+            ),
+            React.createElement(
+                "tr",
+                null,
+                React.createElement(
+                    "td",
+                    { align: "right" },
+                    "\u0410\u0432\u0442\u043E\u0440\u0438:"
+                ),
+                React.createElement(
+                    "td",
+                    null,
+                    "\u041A\u0440\u0430\u0432\u0446\u043E\u0432\u0430, \u041F\u0440\u0438\u0434\u0430\u0442\u043E\u043A."
+                )
+            ),
+            React.createElement(
+                "tr",
+                null,
+                React.createElement(
+                    "td",
+                    { align: "right" },
+                    "\u0416\u0430\u043D\u0440:"
+                ),
+                React.createElement(
+                    "td",
+                    null,
+                    "\u0410\u0437\u0431\u0443\u043A\u0430."
+                )
+            ),
+            React.createElement(
+                "tr",
+                null,
+                React.createElement(
+                    "td",
+                    { align: "right" },
+                    "\u041A\u0456\u043B\u044C\u043A\u0456\u0441\u0442\u044C \u0441\u0442\u043E\u0440\u0456\u043D\u043E\u043A:"
+                ),
+                React.createElement(
+                    "td",
+                    null,
+                    "96 \u0441\u0442\u043E\u0440\u0456\u043D\u043E\u043A."
+                )
+            )
+        )
+    ),
+    React.createElement(
+        "div",
+        null,
+        "______________________________________________________________________________________________________________________________________________________________________________________________________________"
+    ),
+    React.createElement(
+        "table",
+        null,
+        React.createElement(
+            "tbody",
+            null,
+            React.createElement(
+                "tr",
+                null,
+                React.createElement(
+                    "td",
+                    { rowspan: "5" },
+                    React.createElement("img", { src: "img2.png", width: "80%", height: "80%" })
+                ),
+                React.createElement(
+                    "td",
+                    null,
+                    "\u041D\u0430\u0437\u0432\u0430\u043D\u0438\u0435 \u0433\u0440\u0443\u043F\u043F\u044B:"
+                ),
+                React.createElement(
+                    "td",
+                    null,
+                    "Infornal Fuck\u044A"
+                )
+            ),
+            React.createElement(
+                "tr",
+                null,
+                React.createElement(
+                    "td",
+                    null,
+                    "\u0418\u043D\u0444\u043E\u0440\u043C\u0430\u0446\u0438\u044F \u043E\u0431 \u0443\u0447\u0430\u0441\u0442\u043D\u0438\u043A\u0430\u0445 \u0433\u0440\u0443\u043F\u043F\u044B:"
+                ),
+                React.createElement(
+                    "td",
+                    null,
+                    React.createElement(
+                        "ul",
+                        null,
+                        React.createElement(
+                            "li",
+                            null,
+                            React.createElement(
+                                "div",
+                                null,
+                                "\u0410\u043B\u0435\u043A\u0441\u0435\u0439 \u041C\u0435\u0434\u0432\u0435\u0434\u0435\u0432 (\u0432\u043E\u043A\u0430\u043B, \u0441\u043E\u043B\u043E-\u0433\u0438\u0442\u0430\u0440\u0430),"
+                            )
+                        ),
+                        React.createElement(
+                            "li",
+                            null,
+                            React.createElement(
+                                "div",
+                                null,
+                                "\u0410\u043B\u0435\u043A\u0441\u0435\u0439 \u041A\u0438\u0440\u044C\u044F\u043D\u0435\u043D\u043A\u043E (\u0440\u0438\u0442\u043C-\u0433\u0438\u0442\u0430\u0440\u0430),"
+                            )
+                        ),
+                        React.createElement(
+                            "li",
+                            null,
+                            React.createElement(
+                                "div",
+                                null,
+                                "\u0421\u0435\u0432\u0435\u0440\u044C\u044F\u043D \u0428\u0430\u0445\u043E\u0432 (\u0431\u0430\u0441),"
+                            )
+                        ),
+                        React.createElement(
+                            "li",
+                            null,
+                            React.createElement(
+                                "div",
+                                null,
+                                "\u0410\u043D\u0430\u0441\u0442\u0430\u0441\u0438\u044F \u041A\u043E\u0432\u0430\u043B\u0451\u0432\u0430 (\u043A\u043B\u0430\u0432\u0438\u0448\u0438),"
+                            )
+                        ),
+                        React.createElement(
+                            "li",
+                            null,
+                            React.createElement(
+                                "div",
+                                null,
+                                "\u0421\u0435\u0440\u0433\u0435\u0439 (\u0443\u0434\u0430\u0440\u043D\u044B\u0435)."
+                            )
+                        )
+                    )
+                )
+            ),
+            React.createElement(
+                "tr",
+                null,
+                React.createElement(
+                    "td",
+                    null,
+                    "\u041D\u0430\u0437\u0432\u0430\u043D\u0438\u0435 \u0430\u043B\u044C\u0431\u043E\u043C\u043E\u0432:"
+                ),
+                React.createElement(
+                    "td",
+                    null,
+                    React.createElement(
+                        "ul",
+                        null,
+                        React.createElement(
+                            "li",
+                            null,
+                            React.createElement(
+                                "div",
+                                null,
+                                React.createElement("img", { src: "1.png", width: "10%", height: "10%", align: "center" }),
+                                " \u2014 \u0410\u043B\u043A\u043E\u0433\u043E\u043B\u0438\u0437\u043C \u0421\u0438\u043B\u044C\u043D\u0435\u0435 \u0413\u0440\u0430\u0432\u0438\u0442\u0430\u0446\u0438\u0438,"
+                            )
+                        ),
+                        React.createElement(
+                            "li",
+                            null,
+                            React.createElement(
+                                "div",
+                                null,
+                                React.createElement("img", { src: "2.png", width: "10%", height: "10%", align: "center" }),
+                                " \u2014 \u041A\u043B\u0438\u043D\u0438\u043A\u0430 \u0412\u0430\u043B\u0445\u0430\u043B\u043B\u0430,"
+                            )
+                        ),
+                        React.createElement(
+                            "li",
+                            null,
+                            React.createElement(
+                                "div",
+                                null,
+                                React.createElement("img", { src: "3.png", width: "10%", height: "10%", align: "center" }),
+                                " \u2014 \u041C\u0430\u043D\u0451\u0432\u0440\u044B \u0441\u0435\u0432\u0435\u0440\u043D\u043E\u0439 \u0430\u043B\u043A\u043E\u0434\u0438\u0432\u0438\u0437\u0438\u0438,"
+                            )
+                        ),
+                        React.createElement(
+                            "li",
+                            null,
+                            React.createElement(
+                                "div",
+                                null,
+                                React.createElement("img", { src: "4.png", width: "10%", height: "10%", align: "center" }),
+                                " \u2014 \u041F\u043E\u043B\u043D\u043E\u0447\u044C. XXI \u0432\u0435\u043A,"
+                            )
+                        ),
+                        React.createElement(
+                            "li",
+                            null,
+                            React.createElement(
+                                "div",
+                                null,
+                                React.createElement("img", { src: "5.png", width: "10%", height: "10%", align: "center" }),
+                                " \u2014 \u041A\u0440\u0430\u0439 \u0440\u043E\u0434\u043D\u043E\u0439,"
+                            )
+                        ),
+                        React.createElement(
+                            "li",
+                            null,
+                            React.createElement(
+                                "div",
+                                null,
+                                React.createElement("img", { src: "6.png", width: "10%", height: "10%", align: "center" }),
+                                " \u2014 \u0423\u0436\u0435 \u043D\u0435 \u0442\u043E."
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    ),
+    React.createElement(
+        "div",
+        null,
+        "______________________________________________________________________________________________________________________________________________________________________________________________________________"
+    ),
+    React.createElement(
+        "div",
+        null,
+        React.createElement("img", { src: "img3.jpg", width: "30%", height: "30%" })
+    ),
+    React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            { style: { fontSize: 57 } },
+            "\u0421\u0430\u043B\u0430\u0442 \u0438\u0437 \u0420\u041E\u041B\u041B\u0422\u041E\u041D\u0410"
+        )
+    ),
+    React.createElement(
+        "div",
+        { style: { marginLeft: -340 } },
+        React.createElement(
+            "h1",
+            { style: { fontSize: 37 } },
+            "\u0418\u043D\u0433\u0440\u0435\u0434\u0438\u0435\u043D\u0442\u044B"
+        ),
+        React.createElement(
+            "ul",
+            null,
+            React.createElement(
+                "li",
+                { style: { marginLeft: -110 } },
+                "1 \u043F\u0430\u0447\u043A\u0430 (85 \u0433\u0440) \u0440\u043E\u043B\u043B\u0442\u043E\u043D\u0430"
+            ),
+            React.createElement(
+                "li",
+                { style: { marginLeft: -204 } },
+                "1 \u041C\u043E\u0440\u043A\u043E\u0432\u044C"
+            ),
+            React.createElement(
+                "li",
+                { style: { marginLeft: -171 } },
+                "1 \u043B\u0443\u043A \u0440\u0435\u043F\u0447\u0430\u0442\u044B\u0439"
+            ),
+            React.createElement(
+                "li",
+                { style: { marginLeft: -232 } },
+                "2 \u044F\u0439\u0446\u0430"
+            ),
+            React.createElement(
+                "li",
+                { style: { marginLeft: -153 } },
+                "\u043F\u043E \u0432\u043A\u0443\u0441\u0443 \u041C\u0430\u0439\u043E\u043D\u0435\u0437"
+            ),
+            React.createElement(
+                "li",
+                { style: { marginLeft: -179 } },
+                "\u043F\u043E \u0432\u043A\u0443\u0441\u0443 \u0421\u043E\u043B\u044C"
+            )
+        )
+    ),
+    React.createElement(
+        "h1",
+        null,
+        "\u041A\u0430\u043A \u043F\u0440\u0438\u0433\u043E\u0442\u043E\u0432\u0438\u0442\u044C"
+    ),
+    React.createElement(
+        "p",
+        { align: "left" },
+        React.createElement(
+            "b",
+            { style: { fontSize: 60 } },
+            "1."
+        ),
+        React.createElement(
+            "b",
+            { style: { fontSize: 30 } },
+            "\u0420\u043E\u043B\u043B\u0442\u043E\u043D \u0437\u0430\u043B\u0438\u0442\u044C \u043A\u0438\u043F\u044F\u0442\u043A\u043E\u043C \u0432\u043C\u0435\u0441\u0442\u0435 \u0441 \u0441\u0443\u0445\u043E\u0439 \u043F\u0440\u0438\u043F\u0440\u0430\u0432\u043E\u0439. \u0412\u043E\u0434\u044B \u043D\u0435\u043C\u043D\u043E\u0433\u043E. \u0415\u0441\u043B\u0438 \u0431\u0443\u0434\u0435\u0442 \u043C\u043D\u043E\u0433\u043E, \u043F\u043E\u0442\u043E\u043C \u043C\u043E\u0436\u043D\u043E \u0441\u043B\u0438\u0442\u044C. \u041E\u0431\u0436\u0430\u0440\u0438\u0442\u044C \u043B\u0443\u043A \u0440\u0435\u043F\u0447\u0430\u0442\u044B\u0439 \u0438 \u043C\u043E\u0440\u043A\u043E\u0432\u044C, \u044F\u0439\u0446\u043E \u043D\u0430\u0442\u0435\u0440\u0435\u0442\u044C \u043D\u0430 \u043A\u0440\u0443\u043F\u043D\u043E\u0439 \u0442\u0451\u0440\u043A\u0435, \u0432\u0441\u0435 \u0434\u043E\u0431\u0430\u0432\u0438\u0442\u044C \u043A \u0440\u043E\u043B\u043B\u0442\u043E\u043D\u0443, \u043F\u043E\u0441\u043E\u043B\u0438\u0442\u044C \u043F\u043E \u0432\u043A\u0443\u0441\u0443, \u0434\u043E\u0431\u0430\u0432\u0438\u0442\u044C \u043C\u0430\u0439\u043E\u043D\u0435\u0437, \u043F\u0435\u0440\u0435\u043C\u0435\u0448\u0430\u0442\u044C. \u041F\u0440\u0438\u044F\u0442\u043D\u043E\u0433\u043E \u0430\u043F\u043F\u0435\u0442\u0438\u0442\u0430 \uD83D\uDCAF!"
+        )
+    ),
+    React.createElement("img", { align: "left", src: "img4.jpg", width: "20%", height: "20%" }),
+    React.createElement(
+        "div",
+        null,
+        "______________________________________________________________________________________________________________________________________________________________________________________________________________"
+    )
+), document.getElementById("div_id_1"));
